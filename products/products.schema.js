9cb82c2718b2461a4c5a0ad1b070215c2d59f73d
AhshanHabib26 <habib.net27@gmail.com>
2024-03-25 11:46:25 +0600

    Vercel Added

--- products/products.schema.js
+++ products/products.schema.js
@@ -1,35 +1,48 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    price: {
+      type: String,
+      required: [true, "Price is required"],
+    },
+    ratings: {
+      type: String,
+      required: [true, "Ratings are required"],
+    },
+    brand: {
+      type: String,
+      required: [true, "Brand is required"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+    },
+    image: {
+      type: String,
+      required: [true, "Image URL is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    flashSale: {
+      type: Boolean,
+      default: false,
+    },
+    flashSaleOffer: {
+      type: String,
+    },
+    discount: {
+      type: String,
+    },
   },
-  price: {
-    type: String,
-    required: [true, "Price is required"],
-  },
-  ratings: {
-    type: String,
-    required: [true, "Ratings are required"],
-  },
-  brand: {
-    type: String,
-    required: [true, "Brand is required"],
-  },
-  category: {
-    type: String,
-    required: [true, "Category is required"],
-  },
-  image: {
-    type: String,
-    required: [true, "Image URL is required"],
-  },
-  description: {
-    type: String,
-    required: [true, "Description is required"],
-  },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
